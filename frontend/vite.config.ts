77deaac264a279c7712d755e90a53109224570d3
Ally-Andre <u23525984@tuks.co.za>
2026-05-19 14:27:58 +0200

    chore: configure Tailwind and shadcn UI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
+import path from "path" //node helper for building files safely
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
+  resolve: { //telsl vite how to understand the imports
+    alias:{ //creates shortcut name for folder
+      "@":path.resolve(__dirname,"./src"),//@is shortcut for src folder
+    },
+  },
 })
