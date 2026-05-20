a1dd16f9abacf61b0761bebaef8cd2b2cc82731b
Ally-Andre <u23525984@tuks.co.za>
2026-05-21 00:50:51 +0200

    Feature: Registration page w/ validation and testing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
 import LoginPage from './domains/LoginPage';
+import RegisterPage from './domains/RegisterPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />}/>
         <Route path="/login" element={<LoginPage />}/>
+        <Route path="/register" element={<RegisterPage/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
