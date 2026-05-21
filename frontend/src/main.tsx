240a465fd1d2fd25bcc78e03ddf8660626bc7a33
rachelclifford27 <u24647374@tuks.co.za>
2026-05-21 11:01:21 +0200

    moved calendar and dashboard pages into domains folder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,18 +3,22 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import DashboardPage from '@/features/dashboard/DashboardPage'
-import CalendarPage from '@/features/calendar/CalendarPage'
+import DashboardPage from '@/domains/DashboardPage.tsx'
+import CalendarPage from '@/domains/CalendarPage.tsx'
 import LoginPage from './domains/LoginPage'
 import RegisterPage from './domains/RegisterPage.tsx'
 import StickerAlbumPage from './domains/StickerAlbumPage'
 import StickerDetailPage from './domains/StickerDetailPage'
+import {initAuthListener} from './features/auth/auth.service'
+
+initAuthListener()
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<DashboardPage />}/>
+                <Route path="/domains/dashboard" element={<DashboardPage />}/>
                 <Route path="/dev" element={<App />}/>
                 <Route path="/calendar" element={<CalendarPage />}/>
                 <Route path="/login" element={<LoginPage />}/>
