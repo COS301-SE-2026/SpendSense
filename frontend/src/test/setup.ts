22ab00820938c867286515b5b10c002e11fd8d0e
Ally-Andre <u23525984@tuks.co.za>
2026-05-21 14:27:38 +0200

    feature: payment form and testing

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,7 +1,5 @@
 import "@testing-library/jest-dom";
 import { expect } from "vitest";
-import { beforeEach,afterEach } from "node:test";
-import {vi} from "vitest"
 import * as matcherExtensions from "@testing-library/jest-dom/matchers";
 
 
