d46c0242dbe6208914ec3683529e21177b0d8ea3
kahlanhgrmn <kahlanhagerman@gmail.com>
2026-05-21 03:56:03 +0200

    fix: merge conflicts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,20 +4,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import DashboardPage from '@/features/dashboard/DashboardPage'
+import CalendarPage from '@/features/calendar/CalendarPage'
 import LoginPage from './domains/LoginPage'
+import RegisterPage from './domains/RegisterPage.tsx'
 import StickerAlbumPage from './domains/StickerAlbumPage'
 import StickerDetailPage from './domains/StickerDetailPage'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DashboardPage />}/>
-        <Route path="/dev" element={<App />}/>
-        <Route path="/login" element={<LoginPage />}/>
-        <Route path="/stickers" element={<StickerAlbumPage />}/>
-        <Route path="/stickers/:badgeId" element={<StickerDetailPage />}/>
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<DashboardPage />}/>
+                <Route path="/dev" element={<App />}/>
+                <Route path="/calendar" element={<CalendarPage />}/>
+                <Route path="/login" element={<LoginPage />}/>
+                <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/stickers" element={<StickerAlbumPage />}/>
+                <Route path="/stickers/:badgeId" element={<StickerDetailPage />}/>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
