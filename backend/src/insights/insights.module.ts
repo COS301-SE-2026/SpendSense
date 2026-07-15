0145f66223ca8c56d2190c0736c62f85e028c52b
Morgan-Wat <Morgz0911@gmail.com>
2026-07-15 10:01:45 +0200

    #186 FIX backend crashing due to a missed import inn the insights service controller

--- backend/src/insights/insights.module.ts
+++ backend/src/insights/insights.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InsightsController } from './insights.controller';
 import { InsightsService } from './insights.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [InsightsController],
   providers: [InsightsService]
 })
