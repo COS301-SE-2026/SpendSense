17669400d17eb5139c5504e1bc46c470163b33bf
Morgan-Wat <Morgz0911@gmail.com>
2026-07-15 09:30:20 +0200

    #186 Created insights backend module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { RemindersModule } from './reminders/reminders.module';
 import { CategoriesModule } from './categories/categories.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { PaymentOccurrencesModule } from './payment-occurrences/payment-occurrences.module';
+import { InsightsModule } from './insights/insights.module';
 
 @Module({
   imports: [
@@ -30,6 +31,7 @@ import { PaymentOccurrencesModule } from './payment-occurrences/payment-occurren
     CategoriesModule,
     NotificationsModule,
     PaymentOccurrencesModule,
+    InsightsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
