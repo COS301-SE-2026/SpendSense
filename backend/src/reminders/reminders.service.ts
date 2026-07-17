d17ecb08d876f40c80847b0ec8c2f6ba0c7f6eb9
kahlanhgrmn <kahlanhagerman@gmail.com>
2026-07-17 20:38:31 +0200

    feat: implemented RemindersService get and update reminder prefs

---
+++ backend/src/reminders/reminders.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { UsersService } from 'src/users/users.service'
+import { UpdateReminderPreferencesDto } from './dto/update-reminder-preferences.dto'
+import type { AuthUser } from 'src/auth/types/auth-user.type'
+
+@Injectable()
+export class ReminderService{
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly usersService: UsersService,
+    ){}
+
+    async getReminderPreferences(authUser: AuthUser){
+        const user = await this.usersService.findOrCreateUser(authUser);
+
+        return user.notificationPreference;
+    }
+
+    async  updateReminderPreferences(authUser: AuthUser, dto: UpdateReminderPreferencesDto,){
+        const user = await this.usersService.findOrCreateUser(authUser);
+
+        return this.prisma.notificationPreference.update({
+            where: {userId: user.id},
+            data:dto,
+            select: this.reminderPreferenceSelect
+        });
+    }
+
+    private readonly reminderPreferenceSelect={
+        quietHoursStart: true,
+        quietHoursEnd: true,
+        emailEnabled: true,
+        pushEnabled: true,
+        inAppEnabled: true,
+        smsEnabled: true,
+        defaultReminderDaysBefore: true,
+    };
+}
