c9bfa03cb14d407e935720d38a0d09a08db76fac
Ally-Andre <u23525984@tuks.co.za>
2026-07-25 00:41:25 +0200

    #249 - update notififcations module

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import {NotificationsController} from './notifications.controller';
 import {NotificationsService} from './notifications.service';
+import {AuthModule} from '../auth/auth.module';
+import {UsersModule} from '../users/users.module';
 
 // NotificationsModule: in app notification management
 // TODO: implement NotificationsController and NotificationsService
@@ -10,6 +12,7 @@ import {NotificationsService} from './notifications.service';
 // PATCH /api/v1/notifications/:id
 
 @Module({
+    imports:[AuthModule,UsersModule],
     controllers:[NotificationsController],
     providers:[NotificationsService],
     exports:[NotificationsService],
