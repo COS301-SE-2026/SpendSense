6ab716572b791fd463181c144c6b44e8409554bc
kahlanhgrmn <kahlanhagerman@gmail.com>
2026-07-25 00:24:14 +0200

    feat: added SchedulerController with the protected manual trigger endpoint

--- backend/src/scheduler/scheduler.module.ts
+++ backend/src/scheduler/scheduler.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { SchedulerService } from './scheduler.service';
 import { RemindersModule } from '../reminders/reminders.module';
 import { PaymentOccurrencesModule } from '../payment-occurrences/payment-occurrences.module';
+import { SchedulerController } from './scheduler.controller';
 
 @Module({
   imports: [RemindersModule, PaymentOccurrencesModule],
   exports: [SchedulerService],
   providers: [SchedulerService],
+  controllers: [SchedulerController],
 })
 export class SchedulerModule {}
