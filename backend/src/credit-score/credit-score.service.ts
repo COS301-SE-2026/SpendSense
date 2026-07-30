f3da9bc64f766dce08ba9cedec266d6647c75f4d
kahlanhgrmn <kahlanhagerman@gmail.com>
2026-07-30 14:31:46 +0200

    fix: saavings buffer now rounds off to prevent the many decimals like with the credit score

--- backend/src/credit-score/credit-score.service.ts
+++ backend/src/credit-score/credit-score.service.ts
@@ -50,7 +50,9 @@ export class CreditScoreService {
       CREDIT_SCORE_RANGE.MIN + sumValues * CREDIT_SCORE_RANGE.RANGE;
 
     const applicableRisks = await this.determineApplicableRiskCaps(userId);
-    const finalScore = Math.round(Math.min(calculateScore, applicableRisks.cap));
+    const finalScore = Math.round(
+      Math.min(calculateScore, applicableRisks.cap),
+    );
 
     const finalScoreTier = this.determineScoreTier(finalScore);
 
@@ -78,7 +80,7 @@ export class CreditScoreService {
       reasonForRiskCaps: reason,
       creditScore: finalScore,
       creditScoreTier: finalScoreTier,
-      savingsBuffer,
+      savingsBuffer: Math.round(savingsBuffer),
       onTimePaymentCount,
       onLatePaymentCount,
     };
