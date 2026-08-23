a6f34d3660a531c1fe4f95f7c90fe3f1e1df3ce5
Ally-Andre <u23525984@tuks.co.za>
2026-08-23 12:15:32 +0200

    #348 wagers module

---
+++ backend/src/wagers/wagers.module.ts
@@ -0,0 +1,19 @@
+import {Module} from '@nestjs/common';
+import {AuthModule} from '../auth/auth.module';
+import {NotificationsModule} from '../notifications/notifications.module';
+import {PrismaModule} from '../prisma/prisma.module';
+import {UsersModule} from '../users/users.module';
+import {WagersController} from './wagers.controller';
+import {WagersService} from './wagers.service';
+
+@Module({
+    imports:[
+        AuthModule,
+        NotificationsModule,
+        PrismaModule,
+        UsersModule,
+    ],
+    controllers:[WagersController],
+    providers:[WagersService],
+})
+export class WagersModule{}
