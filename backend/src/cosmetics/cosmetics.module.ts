be263aad0d8c73507e756dbf474745729ce6b793
kahlanhgrmn <kahlanhagerman@gmail.com>
2026-08-29 14:37:50 +0200

    feat: added the RewardsModule to cosmetics.module.ts

--- backend/src/cosmetics/cosmetics.module.ts
+++ backend/src/cosmetics/cosmetics.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { RewardModule } from '../rewards/reward.module';
 import { CosmeticsController } from './cosmetics.controller';
 import { CosmeticsService } from './cosmetics.service';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule],
+  imports: [UsersModule, AuthModule, PrismaModule, RewardModule],
   controllers: [CosmeticsController],
   providers: [CosmeticsService],
   exports: [CosmeticsService],
