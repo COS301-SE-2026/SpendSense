58cf245b5de5fecea993302a368117f05932d816
Morgan-Wat <Morgz0911@gmail.com>
2026-08-30 15:39:45 +0200

    #368 payment-occurances.service.ts CreditProfile persistent MISSED and OVERDUE payments handled

--- backend/src/payment-occurrences/payment-occurrences.module.ts
+++ backend/src/payment-occurrences/payment-occurrences.module.ts
@@ -4,9 +4,10 @@ import { PaymentOccurrencesService } from './payment-occurrences.service';
 import { AuthModule } from '../auth/auth.module';
 import { UsersModule } from '../users/users.module';
 import { RewardModule } from '../rewards/reward.module';
+import { CreditScoreModule } from '../credit-score/credit-score.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, RewardModule],
+  imports: [AuthModule, UsersModule, RewardModule, CreditScoreModule],
   controllers: [PaymentOccurrencesController],
   providers: [PaymentOccurrencesService],
   exports: [PaymentOccurrencesService],
