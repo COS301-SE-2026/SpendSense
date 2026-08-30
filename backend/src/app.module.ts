e9e1d94bbb41f6c4f800b4b1f0f2b15753a139f1
rachelclifford27 <u24647374@tuks.co.za>
2026-08-30 20:08:12 +0200

    fixed fault in calendar page and added/modified testing. Missed payments are now reflected correctly.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -22,6 +23,7 @@ import { RewardModule } from './rewards/reward.module';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     PrismaModule,
     AuthModule,
     UsersModule,
