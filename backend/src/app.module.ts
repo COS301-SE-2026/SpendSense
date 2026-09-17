0950c9473b4144b288a835d2bd01a689d8dff57b
kahlanhgrmn <kahlanhagerman@gmail.com>
2026-09-17 15:56:31 +0200

    feat: registered GuidanceModule in AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -24,6 +24,7 @@ import { CosmeticsModule } from './cosmetics/cosmetics.module';
 import { MonthlyWrappedModule } from './wrapped/wrapped.module';
 import { FriendsModule } from './friends/friends.module';
 import { WagersModule } from './wagers/wagers.module';
+import { GuidanceModule } from './guidance/guidance.module';
 
 @Module({
   imports: [
@@ -50,6 +51,7 @@ import { WagersModule } from './wagers/wagers.module';
     MonthlyWrappedModule,
     FriendsModule,
     WagersModule,
+    GuidanceModule,
   ],
   controllers: [AppController],
   providers: [AppService],
